cd080b4664a181809f992566960dd0e78c803aeb
goldstardev2002 <goldstardev2002@gmail.com>
2022-02-14 14:21:10 +0900

    Contract compiler completed

---
+++ scripts/run.js
@@ -0,0 +1,22 @@
+const main = async () => {
+
+      const coffeeContractFactory = await hre.ethers.getContractFactory('CoffeePortal');
+      const coffeeContract = await coffeeContractFactory.deploy();
+    
+      await coffeeContract.deployed();
+    
+     console.log("Coffee Contract deployed to:", coffeeContract.address);
+    };
+    
+    const runMain = async () => {
+      try {
+        await main();
+        process.exit(0);
+      } catch (error) {
+        console.log(error);
+        process.exit(1);
+      }
+    };
+    
+    runMain();
+    
